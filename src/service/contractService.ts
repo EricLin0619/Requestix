30080115d821cadf332fae6b7efb38fe82dcbbd3
Eric Lin <40213852eric@gmail.com>
2024-12-06 14:30:15 +0800

    fix: use web3 to read contract

--- src/service/contractService.ts
+++ src/service/contractService.ts
@@ -2,6 +2,7 @@ import { writeContract, prepareWriteContract, waitForTransaction, readContract }
 import ABI from "../contracts/ABI.json";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { BytesLike, ethers } from "ethers";
+import Web3 from "web3";
 
 const provider = new ethers.providers.JsonRpcProvider(
   "https://rpc.sepolia.org"
@@ -11,6 +12,10 @@ const signer = new ethers.Wallet(
   provider
 );
 const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`, ABI, signer);
+
+// web3
+const web3 = new Web3(new Web3.providers.HttpProvider("https://rpc.sepolia.org"));
+const web3Contract = new web3.eth.Contract(ABI, process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`);
 
 
 export async function createEventOnChain(metaDataCid: string, maxRegistrations: number, saleStartDate: number, saleEndDate: number) {
@@ -57,21 +62,31 @@ export async function eventCount() {
 //   return events;
 // }
 
+// export async function getAllEvents() {
+//   const count = await readContract({
+//     address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
+//     abi: ABI,
+//     functionName: "eventCount",
+//   }) as number;
+//   console.log(count)
+//   const events: any[] = []
+//   for (let i = 1; i <= count; i++) {
+//     const data = await readContract({
+//       address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
+//       abi: ABI,
+//       functionName: "events",
+//       args: [i],
+//     });
+//     events.push(data);
+//   }
+//   return events;
+// }
+
 export async function getAllEvents() {
-  const count = await readContract({
-    address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
-    abi: ABI,
-    functionName: "eventCount",
-  }) as number;
-  console.log(count)
-  const events: any[] = []
+  const count = await web3Contract.methods.eventCount().call() as number;
+  const events = []
   for (let i = 1; i <= count; i++) {
-    const data = await readContract({
-      address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
-      abi: ABI,
-      functionName: "events",
-      args: [i],
-    });
+    const data = await web3Contract.methods.getEvent(i).call();
     events.push(data);
   }
   return events;
